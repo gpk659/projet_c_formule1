1aab314579ce31671dd96e736c1728a40497d576
bari <pyck.greg@hotmail.fr>
2016-10-13 15:35:03 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+//les librairies
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//declaration de toutes les variables et tableaux
+
+
+//mes structures
+struct Pilote{
+  public int pilote_id;
+}
+
+
+//mes fonctions
+double participant(){
+}
+
+double creerTour(){
+}
+
+//fonction principale qui permet de lancer le programme
+int main(){
+  Pilote partant1;
+  /* specification pilote */
+  pilote1.pilote_id=22;
+}
