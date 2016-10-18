93e56c27978cdf8ce2ac24f836bfd8a55813bdd5
bari <pyck.greg@hotmail.fr>
2016-10-18 09:48:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,31 +8,42 @@
 
 
 //mes structures
-struct Pilote{
+struct Pilote {
   int pilote_id;
-  double segment1,segment2,segment3;
-  double tour;
-  double [5][2]={};
+ double segment1,segment2,segment3;
+ double tour;
+  double results[5][3];
+/*
+ * {,,} les id des pilotes
+ * {,,} le segment 1
+ * {,,} le segment 2
+ * {,,} le segment 3
+ * {,,} le tour
+ * 
+ */
   
 };
 
-
+/*
 //mes fonctions
 double participant(){
 }
 
 double creerTour(){
 }
-//calcul des meilleurs temps
+//calcul des meilleurs //////temps
 double meilleurSegment(){
-  
+ // 
 }
 double meilleurTour(){
-}
+}*/
 
 //fonction principale qui permet de lancer le programme
-int main(){
-  Pilote Pilote1;
+int main( ) {
+ struct Pilote Pilote1;
   /* specification pilote */
-  Pilote1.pilote_id=22;
+  //Pilote1.pilote_id=22;
+	Pilote1.results[0][0]={22};
+	printf("id : %d\n", Pilote1.results[0][0]);
+return 0;
 }
