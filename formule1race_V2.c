31bca20abf8ee8a7223bf20dfdcf2336b19f49fd
bari <pyck.greg@hotmail.fr>
2016-10-22 13:54:03 +0200

    Update formule1race_V2.c
    Operation with a structure rather than an array

--- formule1race_V2.c
+++ formule1race_V2.c
@@ -7,27 +7,20 @@
 #define nbParticipants 22
 #define nbTourEssai_1 5
 
-void afficheCoureurs(int *tab_coureur);
-
-void menu_depart(int *tab_coureur);
-
-void essai_libre(int *tab_coureur);
-
 int random_number(int min, int max);
 
 /**
-*
-*
+* Version 2.0
+* @author : grégory
 **/
-
 
 typedef struct {
 	int numVoiture;
 	int *s1,*s2,*s3;
-
 } t_pilote;
 
-t_pilote tab_coureur[nbParticipants]={
+
+t_pilote tb_coureur[nbParticipants]={
 	{44,NULL,NULL,NULL},
 	{6,NULL,NULL,NULL},
 	{5,NULL,NULL,NULL},
@@ -49,9 +42,11 @@ t_pilote tab_coureur[nbParticipants]={
 	{8,NULL,NULL,NULL},
 	{21,NULL,NULL,NULL},
 	{31,NULL,NULL,NULL},
-	{94,NULL,NULL,NULL},
-}
-//fonction random fonctionne!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	{94,NULL,NULL,NULL}
+};
+
+
+//fonction random pour générer un temps aléatoire
     int random_number(int min, int max){
         static int rand_is_seeded = 0;
         if(!rand_is_seeded){
@@ -61,56 +56,54 @@ t_pilote tab_coureur[nbParticipants]={
         return rand()%(max-min+1)+min;
     }
 
-void afficheCoureurs(int *tab_coureur){
-	printf("\nListe des Particpants engagés dans la course : \n");
-	int i;
-	for(i=0; i < nbParticipants ; i++){
-		printf("Id : ");
-		printf("%d",tab_coureur[i]);	
-		printf("\n");
-	}
 
+//fonction pour afficher tous les coureurs
+void afficheCoureurs(){
+	printf("Test affichage coureur avec struct\n");
+	int i;
+	for(i=0; i<nbParticipants; i++){
+		printf("Id : %d .\n",tb_coureur[i].numVoiture );
+	}	
 }
 
 
-void essai_libre(int *tab_coureur){//fonction pour les essais libres
+void essai_libre(){//fonction pour les essais libres
 	int choix_essai;
 	int compteur =0;
 	//system("clear");
 	printf("-----------------------------------------------------\n");
-	printf("- 		Date : 05/02/17 PM : essai libre (1h30)     -\n");
+	printf("- 	   Date : 05/02/17 PM : essai libre (1h30)      -\n");
 	printf("-----------------------------------------------------\n");
-
 	
 	while(choix_essai < 1 || choix_essai >2){
-	
+
 		printf("Choissiez une option pour continuer :\n");
-		printf("1. Commencer le premier essai libre.\n");
-		printf("2. Ne pas commencer l'essai libre, fin du Grand Prix de Forumule 1\n");
-		printf("Votre choix ? ");
+		printf(" 1. Commencer le premier essai libre.\n");
+		printf(" 2. Ne pas commencer l'essai libre, fin du Grand Prix de Forumule 1\n");
+		printf(" Votre choix ? ");
 		scanf("%d",&choix_essai);
 	
 	switch(choix_essai){
+
 		case 1:
 			printf("L'essai de ce Vendredi 05/02/17 va commencer...\n");
-
 			for (int i=0; i<nbTourEssai_1;i++){
 
-				for(compteur=0; compteur<nbParticipants; compteur++){
-					printf("Coureur : %d va commencer son tour. Le coureur a effecuté son tour en %d.\n",tab_coureur[compteur],random_number(100,300));	
-				}
-				printf("Tour n°%d.\n",i+1);
+				printf("Tour n°%d.\n\n",i+1);
 				getchar();
-
+				for(compteur=0; compteur<nbParticipants; compteur++){
+					printf("Coureur : %d va commencer son tour. Le coureur a effecuté son tour en %d.\n",tb_coureur[compteur].numVoiture,random_number(100,300));	
+				}
 			}
-			printf("%d tours effecuté lors de ce premier essai.\n",nbTourEssai_1);
-			printf("\nEssai libre terminé...");
+			printf("%d tours effecuté lors de ce premier essai.\nEssai libre terminé...\n",nbTourEssai_1);
 			getchar();
 			break;
+
 		case 2:
 			printf("Le Grand Prix de Forumule 1 n'as pas commencé, fin de la course...\n");
 			exit(0);
 			break;
+
 		default:
 			printf("Erreur...Veuillez entrer 1 ou 2 !\n");
 			break;
@@ -120,14 +113,11 @@ void essai_libre(int *tab_coureur){//fonction pour les essais libres
 }
 
 
-
-void menu_depart(int *tab_coureur){//fonction de départ
+void menu_depart(){//fonction de départ
 	int choix;
 
 	printf("\n\n\nProjet OS Pratique présenté par Grégory - Amine - Joel - Nadia\n\n\n");
-/**
-*
-**/
+
 	printf("\n***************************************************\n");
 	printf(" *           Grand Prix de Forumule 1              *\n");
 	printf(" *              Lieu : Nürburgring                 *\n");
@@ -165,25 +155,23 @@ void menu_depart(int *tab_coureur){//fonction de départ
 		switch(choix){
 			case 1:
 				printf("Vous allez commencer la course\n");
+				int AffichCour;
+				printf("Voulez vous afficher tous les Particpants de la course ?\n");
+				printf(" 1. OUI - 2. NON\n");
+				printf("Votre choix ?");
+				scanf("%d",&AffichCour);
 					
-					int AffichCour;
-					printf("Voulez vous afficher tous les Particpants de la course ?\n");
-					printf(" 1. OUI - 2. NON\n");
-					
-					printf("Votre choix ?");
-					scanf("%d",&AffichCour);
-					
-					switch(AffichCour){//switch pour afficher les coureurs ou non
-						case 1:
-							afficheCoureurs(tab_coureur);
-							essai_libre(tab_coureur);
-							break;
-						case 2:
-							printf("La liste des coureurs n'est pas affichée...\n");
-							essai_libre(tab_coureur);
-							break;
-						default:
-							printf("Erreur! Veuillez choisir entre '1' et '2'...");
+				switch(AffichCour){//switch pour afficher les coureurs ou non
+					case 1:
+						afficheCoureurs();
+						essai_libre();
+						break;
+					case 2:
+						printf("La liste des coureurs n'est pas affichée...\n");
+						essai_libre();
+						break;
+					default:
+						printf("Erreur! Veuillez choisir entre '1' et '2'...");
 					}//fin du switch affichCoureurs ou non 
 				break;//fin du choix 1 pour commencer la course
 			case 2:
@@ -194,14 +182,14 @@ void menu_depart(int *tab_coureur){//fonction de départ
 				printf("Choix incorrect...");
 				break;
 		}
-
 	}
-
 }
 
+/**
+* @author : grégory
+**/
+
 int main(){
-	int tab_coureur [nbParticipants]={44,6,5,7,3,33,19,77,11,27,26,55,14,22,9,12,20,30,8,21,31,94};
-	//int compteur=0;
-	menu_depart(tab_coureur);
-	//afficheCoureurs(tab_coureur);
+	
+	menu_depart();
 }
